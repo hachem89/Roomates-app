56624620be679a85ea435666b6bafd779eaa829e
Meddeb Hachem <68928285+hachem89@users.noreply.github.com>
2025-07-29 15:48:38 +0100

    Add create house endpoint

--- backend/src/controllers/house.controller.ts
+++ backend/src/controllers/house.controller.ts
@@ -1,50 +1,48 @@
 import { Request, Response } from "express";
 import { asyncHandler } from "../middlewares/asyncHandler.middleware";
+import { createHouseSchema } from "../validation/house.validation";
+import { HTTPSTATUS } from "../constants/httpStatus.constant";
+import { createHouseService } from "../services/house.service";
 
 export const createHouseController = asyncHandler(
-    async(req:Request, res:Response)=>{
-            
-    }
-)
+  async (req: Request, res: Response) => {
+    const body = createHouseSchema.parse(req.body);
+
+    const userId = req.user?._id;
+
+    const { house } = await createHouseService(userId, body);
+
+    return res.status(HTTPSTATUS.CREATED).json({
+      message: "House Created Successfully",
+      house,
+    });
+  }
+);
 
 export const getHouseByIdController = asyncHandler(
-    async(req:Request, res:Response)=>{
-        
-    }
-)
+  async (req: Request, res: Response) => {}
+);
 
 export const getAllHousesUserIsMemberController = asyncHandler(
-    async(req:Request, res:Response)=>{
-        
-    }
-)
+  async (req: Request, res: Response) => {}
+);
 
 export const getHouseMembersController = asyncHandler(
-    async(req:Request, res:Response)=>{
-        
-    }
-)
+  async (req: Request, res: Response) => {}
+);
 
 export const getHouseAnalyticsController = asyncHandler(
-    async(req:Request, res:Response)=>{
-        
-    }
-)
+  async (req: Request, res: Response) => {}
+);
 
 export const updateHouseByIdController = asyncHandler(
-    async(req:Request, res:Response)=>{
-        
-    }
-) 
+  async (req: Request, res: Response) => {}
+);
 
 export const changeHouseMemberRoleController = asyncHandler(
-    async(req:Request, res:Response)=>{
-        
-    }
-) 
+  async (req: Request, res: Response) => {}
+);
 
 export const deleteHouseByIdController = asyncHandler(
-    async(req:Request, res:Response)=>{
-        
-    }
-)
+  async (req: Request, res: Response) => {}
+);
---
+++ backend/src/services/house.service.ts
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+import { Roles } from "../constants/role.constant";
+import HouseModel from "../models/house.model";
+import MemberModel from "../models/member.model";
+import RoleModel from "../models/roles-permission.model";
+import UserModel from "../models/user.model";
+import { NotFoundException } from "../utils/appError";
+
+export const createHouseService = async (
+  userId: string,
+  body: {
+    name: string;
+    description?: string | undefined;
+  }
+) => {
+  const { name, description } = body;
+
+  const user = await UserModel.findById(userId);
+
+  if (!user) {
+    throw new NotFoundException("User Not Found");
+  }
+
+  const ownerRole = await RoleModel.findOne({ name: Roles.OWNER });
+  if (!ownerRole) {
+    throw new NotFoundException("Owner Role Not Found");
+  }
+
+  const house = new HouseModel({
+    name,
+    description,
+    owner: user._id,
+  });
+
+  await house.save();
+
+  const member = new MemberModel({
+    userId: user._id,
+    houseId: house._id,
+    role: ownerRole._id,
+    joinedAt: new Date(),
+  });
+
+  await member.save();
+
+  user.currentHouse = house._id as mongoose.Types.ObjectId;
+  await user.save();
+
+  return {
+    house,
+  };
+};
