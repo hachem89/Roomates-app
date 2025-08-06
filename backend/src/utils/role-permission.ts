c234ca2ef2c6a46957dbefee10cd483760e1c030
Meddeb Hachem <68928285+hachem89@users.noreply.github.com>
2025-08-07 00:35:56 +0100

    Update RolePermissions

--- backend/src/utils/role-permission.ts
+++ backend/src/utils/role-permission.ts
@@ -1,7 +1,11 @@
-import {Permissions, PermissionType, RoleType } from "../constants/role.constant";
+import {
+  Permissions,
+  PermissionType,
+  RoleType,
+} from "../constants/role.constant";
 
 export const RolePermissions: Record<RoleType, Array<PermissionType>> = {
-    OWNER: [
+  OWNER: [
     Permissions.CREATE_HOUSE,
     Permissions.EDIT_HOUSE,
     Permissions.DELETE_HOUSE,
@@ -11,14 +15,19 @@ export const RolePermissions: Record<RoleType, Array<PermissionType>> = {
     Permissions.CHANGE_MEMBER_ROLE,
     Permissions.REMOVE_MEMBER,
 
+    Permissions.CREATE_CLEANING_TASK,
+    Permissions.EDIT_CLEANING_TASK,
+    Permissions.DELETE_CLEANING_TASK,
+
     Permissions.VIEW_ONLY,
   ],
   ADMIN: [
     Permissions.ADD_MEMBER,
     Permissions.MANAGE_HOUSE_SETTINGS,
+
+    Permissions.CREATE_CLEANING_TASK,
+
     Permissions.VIEW_ONLY,
   ],
-  MEMBER: [
-    Permissions.VIEW_ONLY
-  ],
-} 
+  MEMBER: [Permissions.VIEW_ONLY],
+};
